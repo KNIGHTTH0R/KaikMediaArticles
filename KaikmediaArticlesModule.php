63e2a2a1b024c5e887851a54c6556e553cadbe5b
Kaik <kapitanogor@googlemail.com>
2017-12-01 17:30:12 +0100

    Module basics to see it in extensions list.

---
+++ KaikmediaArticlesModule.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * KaikMedia ArticlesModule
+ *
+ * @package    KaikmediaArticlesModule
+ * @author     Kaik <contact@kaikmedia.com>
+ * @copyright  KaikMedia
+ * @link       https://github.com/Kaik/KaikMediaArticles.git
+ */
+
+namespace Kaikmedia\ArticlesModule;
+
+use Zikula\Core\AbstractModule;
+
+class KaikmediaArticlesModule extends AbstractModule
+{
+}
